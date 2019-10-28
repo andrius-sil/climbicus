2ebbd87311999c9457b1a75dc199322fb48cae0f
Liucija Latanauskaite <llatanauskaite@gocardless.com>
2019-10-28 08:58:47 +0000

    separate predictor dockerfile and requirements

--- predictor/predictor.py
+++ predictor/predictor.py
@@ -7,7 +7,7 @@ from keras.models import load_model
 from PIL import Image
 
 model_name = "castle_30_vgg_fine_tuned.h5"
-model_path = os.path.join("/opt/project/model_training/", model_name)  # ideally woudl get rid of this opt/project
+model_path = os.path.join("/opt/project/predictor/", model_name)  # TODO: get rid of this opt/project
 
 # TODO: Keras predicting could be slow, look into faster methods
 # TODO: When run on a server, the model should always be loaded probably?
@@ -25,7 +25,7 @@ def process_image(image_path):
     The input image needs to be a numpy array of shape (150, 150, 3) rescaled by 1.0/255
     """
     img = Image.open(image_path)
-    img = img.resize((150, 150), Image.LANCZOS)
+    img = img.resize((150, 150), Image.LANCZOS)  # high quality, slow method
     img = np.array(img)
     img = img / 255.0
     img = np.expand_dims(img, axis=0)
@@ -35,7 +35,7 @@ def process_image(image_path):
 def load_and_predict(model_path, img):
     """Loads the model and class indices, makes a class prediction for a single image"""
     model = load_model(model_path)
-    class_indices = load_obj("/opt/project/model_training/class_indices.pkl")
+    class_indices = load_obj("/opt/project/predictor/class_indices.pkl")
 
     # predicted_probabilities = model.predict(img)
     predicted_class_index_array = model.predict_classes(img)
@@ -44,7 +44,7 @@ def load_and_predict(model_path, img):
     return predicted_class
 
 
-image_path = os.path.join("/opt/project/model_training/", "data/categories_castle/test/1/DSC_3878.JPG")
+image_path = "/opt/project/predictor/DSC_3878.JPG"
 img = process_image(image_path)
 result = load_and_predict(model_path, img)
 print(f"Predicted class is: {result}")
