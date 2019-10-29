219651b8d48456fd2ea194599681400faa0d9f6e
Liucija Latanauskaite <llatanauskaite@gocardless.com>
2019-10-29 09:11:10 +0000

    predictor moved to server

--- server/predictor/predictor.py
+++ server/predictor/predictor.py
@@ -6,12 +6,14 @@ import numpy as np
 from keras.models import load_model
 from PIL import Image
 
+base_path = "/app/predictor/"    # TODO: set this outside
 model_name = "castle_30_vgg_fine_tuned.h5"
-model_path = os.path.join("/opt/project/predictor/", model_name)  # TODO: get rid of this opt/project
+MODEL_PATH = os.path.join(base_path, model_name)
+CLASS_INDICES_PATH = os.path.join(base_path, "class_indices.pkl")
 
 # TODO: Keras predicting could be slow, look into faster methods
 # TODO: When run on a server, the model should always be loaded probably?
-# TODO: Check what aspect ratiop Keras is using when resizing
+# TODO: Check what aspect ratio Keras is using when resizing
 
 
 def load_obj(path):
@@ -32,19 +34,14 @@ def process_image(image_path):
     return img
 
 
-def load_and_predict(model_path, img):
+def load_and_predict(image_path):
     """Loads the model and class indices, makes a class prediction for a single image"""
-    model = load_model(model_path)
-    class_indices = load_obj("/opt/project/predictor/class_indices.pkl")
+    model = load_model(MODEL_PATH)
+    class_indices = load_obj(CLASS_INDICES_PATH)
 
     # predicted_probabilities = model.predict(img)
+    img = process_image(image_path)
     predicted_class_index_array = model.predict_classes(img)
     predicted_class_index = predicted_class_index_array[0]
     predicted_class = class_indices[predicted_class_index]
     return predicted_class
-
-
-image_path = "/opt/project/predictor/DSC_3878.JPG"
-img = process_image(image_path)
-result = load_and_predict(model_path, img)
-print(f"Predicted class is: {result}")
