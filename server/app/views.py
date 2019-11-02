a37ab8b1aa971219c7339d76ba86f33779c7a911
Liucija Latanauskaite <llatanauskaite@gocardless.com>
2019-11-02 10:39:29 +0000

    server handles request with image

--- server/app/views.py
+++ server/app/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import abort, Blueprint, request
+from predictor.predictor import load_and_predict
 
 main_blueprint = Blueprint("main_blueprint", __name__)
 
@@ -6,3 +7,13 @@ main_blueprint = Blueprint("main_blueprint", __name__)
 @main_blueprint.route("/")
 def hello_world():
     return "Flask Dockerized"
+
+
+@main_blueprint.route("/predict", methods=["GET"])
+def predict():
+    imagefile = request.files.get("image")
+    if imagefile is None:
+        abort(400, description="Image file is missing")
+    predicted_class = load_and_predict(imagefile)
+    response = predicted_class
+    return response
