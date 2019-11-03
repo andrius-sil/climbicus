452d2e1fc9b2bf5844b77e60cbd1315da4d3d3f7
Liucija Latanauskaite <llatanauskaite@gocardless.com>
2019-11-03 11:17:16 +0000

    db: climicus tables added

--- server/app/db.py
+++ server/app/db.py
@@ -13,7 +13,35 @@ DATABASE_CONNECTION_URI = f"postgresql+psycopg2://{user}:{password}@{host}:{port
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+
+
+class Gyms(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+    # routes = db.relationship("Routes", back_populates="gyms")
+
+
+class Routes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    gym_id = db.Column(db.Integer, db.ForeignKey('gyms.id'), nullable=False)
+    class_id = db.Column(db.String(120), unique=True, nullable=False)
+
+    # gyms = db.relationship("Gyms", back_populates="routes")
+
+
+class RouteImages(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    route_id = db.Column(db.Integer, db.ForeignKey('routes.id'), nullable=False)
+    path = db.Column(db.String(120), unique=True, nullable=False)
+
+
+class UserRouteLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    route_id = db.Column(db.Integer, db.ForeignKey('routes.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    gym_id = db.Column(db.Integer, db.ForeignKey('gyms.id'), nullable=False)
+    log_date = db.Column(db.DateTime, unique=True, nullable=False)
