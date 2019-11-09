e93c1b7900d208cd25b3d65f6bbfe133317e1e03
Liucija Latanauskaite <llatanauskaite@gocardless.com>
2019-11-09 11:14:37 +0000

    pass env file in docker compose

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -5,9 +5,9 @@ import os
 
 user = os.environ["POSTGRES_USER"]
 password = os.environ["POSTGRES_PASSWORD"]
-host = "db"
+host = os.environ["POSTGRES_HOST"]
 database_name = os.environ["POSTGRES_DB"]
-port = "5432"
+port = os.environ["POSTGRES_PORT"]
 
 DATABASE_CONNECTION_URI = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database_name}"
 
