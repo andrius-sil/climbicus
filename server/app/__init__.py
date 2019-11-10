8595132c833ba0c32598fa0f55e09249a61e38fc
Liucija Latanauskaite <llatanauskaite@gocardless.com>
2019-11-10 20:18:12 +0000

    create_app with connection as argument

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,28 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 
-
-user = os.environ["POSTGRES_USER"]
-password = os.environ["POSTGRES_PASSWORD"]
-host = os.environ["POSTGRES_HOST"]
-database_name = os.environ["POSTGRES_DB"]
-port = os.environ["POSTGRES_PORT"]
-
-DATABASE_CONNECTION_URI = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database_name}"
 
 db = SQLAlchemy()
 
 
-def create_app():
+def create_app(db_connection_uri):
     app = Flask(__name__)
 
     # Register Blueprints
     from app.views import main_blueprint
+
     app.register_blueprint(main_blueprint)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION_URI
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_connection_uri
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     with app.app_context():
         db.create_all()
--- server/run.py
+++ server/run.py
@@ -1,3 +1,13 @@
+import os
+
 from app import create_app
 
-app = create_app()
+user = os.environ["POSTGRES_USER"]
+password = os.environ["POSTGRES_PASSWORD"]
+host = os.environ["POSTGRES_HOST"]
+database_name = os.environ["POSTGRES_DB"]
+port = os.environ["POSTGRES_PORT"]
+
+DATABASE_CONNECTION_URI = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database_name}"
+
+app = create_app(DATABASE_CONNECTION_URI)
