2eefc752ca36e6a56e91f63e16c4970ba2e161d0
Liucija Latanauskaite <llatanauskaite@gocardless.com>
2019-11-10 21:55:21 +0000

    route status test

--- server/test/test_endpoints.py
+++ server/test/test_endpoints.py
@@ -1,3 +1,6 @@
+from app.models import UserRouteLog
+
+
 def test_index(client):
     r = client.get("/")
     assert b"Flask Dockerized" in r.data
@@ -5,6 +8,12 @@ def test_index(client):
 
 def test_fetch_logbook(client):
     logbook = client.get("/fetch_logbook", data=dict(user_id=1))
-    assert logbook["1"]["grade"] == "7a"
-    assert logbook["1"]["status"] == "red-point"
-    assert logbook["1"]["log_date"] == "2012-03-03 10:10:10"
+    assert logbook.json["1"]["grade"] == "7a"
+    assert logbook.json["1"]["status"] == "red-point"
+    assert logbook.json["1"]["log_date"] == "Sat, 03 Mar 2012 10:10:10 GMT"
+
+
+def test_add_route_status(client, app):
+    client.post("/add_status", data=dict(status="dogged", predicted_class_id=1, user_id=1, gym_id=1))
+    with app.app_context():
+        assert UserRouteLog.query.order_by(UserRouteLog.log_date.desc()).first().status == "dogged"
