c0bf31627d8814041f81cad1ea10422e99d00e5d
Andrius Silinskas <silinskas.andrius@gmail.com>
2019-11-13 21:28:49 +0000

    Send /predict requests from the app

--- app/climbicus/lib/main.dart
+++ app/climbicus/lib/main.dart
@@ -1,7 +1,11 @@
 import 'dart:io';
 
+import 'package:path/path.dart';
+import 'package:async/async.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
+
 
 void main() {
   runApp(
@@ -33,7 +37,33 @@ class ImagePickerScreen extends StatefulWidget {
 }
 
 class ImagePickerState extends State<ImagePickerScreen> {
+  static const BASE_URL = "http://3.11.0.15:5000";
+
   File _image;
+  Future<String> _predictedClassId;
+
+  Future<String> uploadRouteImage(File image) async {
+    var uri = Uri.parse("$BASE_URL/predict");
+    var request = new http.MultipartRequest("POST", uri);
+
+    var stream = new http.ByteStream(DelegatingStream.typed(image.openRead()));
+    var length = await image.length();
+    var multipartFile = new http.MultipartFile(
+      'image',
+      stream,
+      length,
+      filename: basename(image.path)
+    );
+    request.files.add(multipartFile);
+
+    var response = await request.send();
+    if (response.statusCode == 200) {
+      var value = response.stream.bytesToString();
+      return value;
+    } else {
+      throw Exception("request failed with ${response.statusCode}");
+    }
+  }
 
   Future getImage(ImageSource imageSource) async {
     var image = await ImagePicker.pickImage(
@@ -46,16 +76,32 @@ class ImagePickerState extends State<ImagePickerScreen> {
     setState(() {
       _image = image;
       print("Photo size: ${_image.lengthSync()} bytes");
+
+      _predictedClassId = uploadRouteImage(image);
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    var imageWidget = _image == null ? Text('No image selected') : Image.file(_image);
     return Scaffold(
       body: Center(
-        child: _image == null
-            ? Text('No image selected')
-            : Image.file(_image)
+        child: Column(
+          children: <Widget>[
+            FutureBuilder<String>(
+              future: _predictedClassId,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data);
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
+
+                return CircularProgressIndicator();
+              },
+            ),
+            imageWidget,
+        ]),
       ),
       floatingActionButton: Column(
         mainAxisSize: MainAxisSize.min,
