7aea6ca8cd9edfe60de88d1272641abf201f3401
Andrius Silinskas <silinskas.andrius@gmail.com>
2019-11-16 16:55:36 +0000

    Tests for /users/<id>/predict

--- server/tests/test_users.py
+++ server/tests/test_users.py
@@ -12,3 +12,33 @@ def test_add_to_logbook(client, app):
     client.post("/users/1/logbooks/add", data=dict(status="dogged", predicted_class_id=1, gym_id=1))
     with app.app_context():
         assert UserRouteLog.query.filter_by(status="dogged", user_id=1, gym_id=1).one().status == "dogged"
+
+
+def test_predict_no_image(client):
+    resp = client.post("/users/1/predict")
+    assert resp.status_code == 400
+    assert b"Image file is missing" in resp.data
+
+
+def test_predict_with_image(client):
+    data = {"image": open("resources/green_route.jpg", "rb")}
+
+    resp = client.post("/users/1/predict", data=data)
+    assert resp.status_code == 200
+    assert resp.data == b"1"
+
+
+def test_predict_with_invalid_image(client):
+    data = {"image": b"thisIsNotAnImage"}
+
+    resp = client.post("/users/1/predict", data=data)
+    assert resp.status_code == 400
+    assert b"Image file is missing" in resp.data
+
+
+def test_predict_with_corrupt_image(client):
+    data = {"image": open("resources/corrupt_route.jpg", "rb")}
+
+    resp = client.post("/users/1/predict", data=data)
+    assert resp.status_code == 400
+    assert resp.data == b"Not a valid image"
