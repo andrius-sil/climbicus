435e31fa416a40de79085ba985432aa364a01fbb
Liucija Latanauskaite <llatanauskaite@gocardless.com>
2019-11-16 12:01:35 +0000

    init: add user and general blueprint

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -9,9 +9,10 @@ def create_app(db_connection_uri):
     app = Flask(__name__)
 
     # Register Blueprints
-    from app.views import main_blueprint
+    from app.views import user_blueprint, general_blueprint
 
-    app.register_blueprint(main_blueprint)
+    app.register_blueprint(user_blueprint)
+    app.register_blueprint(general_blueprint)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = db_connection_uri
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
