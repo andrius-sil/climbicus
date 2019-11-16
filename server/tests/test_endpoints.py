fd3d20f6b74a21dda05f47cc7e36e8fe5403822e
Liucija Latanauskaite <llatanauskaite@gocardless.com>
2019-11-16 12:04:28 +0000

    tests: udpate urls

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -6,14 +6,14 @@ def test_index(client):
     assert b"Flask Dockerized" in r.data
 
 
-def test_fetch_logbook(client):
-    logbook = client.get("/fetch_logbook", data=dict(user_id=1))
+def test_view_logbook(client):
+    logbook = client.get("/users/1/logbooks/view")
     assert logbook.json["1"]["grade"] == "7a"
     assert logbook.json["1"]["status"] == "red-point"
     assert logbook.json["1"]["log_date"] == "Sat, 03 Mar 2012 10:10:10 GMT"
 
 
-def test_add_route_status(client, app):
-    client.post("/add_status", data=dict(status="dogged", predicted_class_id=1, user_id=1, gym_id=1))
+def test_add_to_logbook(client, app):
+    client.post("/users/1/logbooks/add", data=dict(status="dogged", predicted_class_id=1, gym_id=1))
     with app.app_context():
         assert UserRouteLog.query.filter_by(status="dogged", user_id=1, gym_id=1).one().status == "dogged"
