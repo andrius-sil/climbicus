38584d0f51b24d8fcdc76d103b6174267865451e
Andrius Silinskas <silinskas.andrius@gmail.com>
2019-11-23 12:44:33 +0000

    Removed DB string length constraint

--- server/app/models.py
+++ server/app/models.py
@@ -6,7 +6,7 @@ from app import db
 
 class Users(db.Model):
     id = db.Column(db.Integer, db.Sequence('user_id_seq'), primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
     _password = db.Column(db.String, nullable=False)
 
     @hybrid_property
@@ -23,16 +23,16 @@ class Users(db.Model):
 
 class Gyms(db.Model):
     id = db.Column(db.Integer, db.Sequence('gym_id_seq'), primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String, nullable=False)
 
 
 class Routes(db.Model):
     id = db.Column(db.Integer, db.Sequence('route_id_seq'), primary_key=True)
     gym_id = db.Column(db.Integer, db.ForeignKey('gyms.id'), nullable=False)
     # TODO: class_id and id should have a 1-1 relationship
-    class_id = db.Column(db.String(120), unique=True, nullable=False)
+    class_id = db.Column(db.String, unique=True, nullable=False)
     # TODO: preset list of possible grades
-    grade = db.Column(db.String(120), nullable=False)
+    grade = db.Column(db.String, nullable=False)
 
 
 class RouteImages(db.Model):
@@ -40,8 +40,8 @@ class RouteImages(db.Model):
     route_id = db.Column(db.Integer, db.ForeignKey('routes.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     probability = db.Column(db.Float, nullable=False)
-    model_version = db.Column(db.String(120), nullable=False)
-    path = db.Column(db.String(120), unique=True, nullable=False)
+    model_version = db.Column(db.String, nullable=False)
+    path = db.Column(db.String, unique=True, nullable=False)
 
 
 class UserRouteLog(db.Model):
@@ -49,5 +49,5 @@ class UserRouteLog(db.Model):
     route_id = db.Column(db.Integer, db.ForeignKey('routes.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     gym_id = db.Column(db.Integer, db.ForeignKey('gyms.id'), nullable=False)
-    status = db.Column(db.String(120), nullable=False)
+    status = db.Column(db.String, nullable=False)
     log_date = db.Column(db.DateTime, nullable=False)
