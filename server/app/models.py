3a6e20bcb95958b726d3e1e003722836c130e4c6
Liucija Latanauskaite <llatanauskaite@gocardless.com>
2019-11-24 13:07:55 +0000

    models: undo double quotes

--- server/app/models.py
+++ server/app/models.py
@@ -2,41 +2,41 @@ from app import db
 
 
 class Users(db.Model):
-    id = db.Column(db.Integer, db.Sequence("user_id_seq"), primary_key=True)
+    id = db.Column(db.Integer, db.Sequence('user_id_seq'), primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
 
 class Gyms(db.Model):
-    id = db.Column(db.Integer, db.Sequence("gym_id_seq"), primary_key=True)
+    id = db.Column(db.Integer, db.Sequence('gym_id_seq'), primary_key=True)
     name = db.Column(db.String(120), nullable=False)
 
 
 class Routes(db.Model):
-    id = db.Column(db.Integer, db.Sequence("route_id_seq"), primary_key=True)
-    gym_id = db.Column(db.Integer, db.ForeignKey("gyms.id"), nullable=False)
+    id = db.Column(db.Integer, db.Sequence('route_id_seq'), primary_key=True)
+    gym_id = db.Column(db.Integer, db.ForeignKey('gyms.id'), nullable=False)
     # TODO: class_id and id should have a 1-1 relationship
     class_id = db.Column(db.String(120), unique=True, nullable=False)
     # TODO: preset list of possible grades
     grade = db.Column(db.String(120), nullable=False)
 
     def create_route_predict_response(self):
-        return {"route_id": self.id, "grade": self.grade}
+        return {'route_id': self.id, 'grade': self.grade}
 
 
 class RouteImages(db.Model):
-    id = db.Column(db.Integer, db.Sequence("route_image_id_seq"), primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    user_route_id = db.Column(db.Integer, db.ForeignKey("routes.id"))
-    model_route_id = db.Column(db.Integer, db.ForeignKey("routes.id"), nullable=False)
+    id = db.Column(db.Integer, db.Sequence('route_image_id_seq'), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_route_id = db.Column(db.Integer, db.ForeignKey('routes.id'))
+    model_route_id = db.Column(db.Integer, db.ForeignKey('routes.id'), nullable=False)
     model_probability = db.Column(db.Float, nullable=False)
     model_version = db.Column(db.String(120), nullable=False)
     path = db.Column(db.String(120), unique=True, nullable=False)
 
 
 class UserRouteLog(db.Model):
-    id = db.Column(db.Integer, db.Sequence("user_route_lod_id_seq"), primary_key=True)
-    route_id = db.Column(db.Integer, db.ForeignKey("routes.id"), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    gym_id = db.Column(db.Integer, db.ForeignKey("gyms.id"), nullable=False)
+    id = db.Column(db.Integer, db.Sequence('user_route_lod_id_seq'), primary_key=True)
+    route_id = db.Column(db.Integer, db.ForeignKey('routes.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    gym_id = db.Column(db.Integer, db.ForeignKey('gyms.id'), nullable=False)
     status = db.Column(db.String(120), nullable=False)
     log_date = db.Column(db.DateTime, nullable=False)
