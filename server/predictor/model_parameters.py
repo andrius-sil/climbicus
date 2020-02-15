161bad9d04e14f36fbad31b9844198bdd9a842d1
Liucija Latanauskaite <llatanauskaite@gocardless.com>
2020-02-15 11:03:40 +0000

    model parameters updated to new model

--- server/predictor/model_parameters.py
+++ server/predictor/model_parameters.py
@@ -1,7 +1,7 @@
 import os
 
 BASE_PATH = "/app/predictor/model_files"
-MODEL_FILE_NAME = "castle_30_vgg_fine_tuned.h5"
-MODEL_VERSION = "castle_30_vgg_fine_tuned"
+MODEL_FILE_NAME = "vgg16_0.0001lr0_200s_0.3drop_class_weight_2020-01-12_10-35-29.h5"
+MODEL_VERSION = "vgg16_0.0001lr0_200s_0.3drop_class_weight_2020-01-12_10-35-29"
 MODEL_PATH = os.path.join(BASE_PATH, MODEL_FILE_NAME)
-CLASS_INDICES_PATH = os.path.join(BASE_PATH, "class_indices.pkl")
+CLASS_INDICES_PATH = os.path.join(BASE_PATH, "class_indices_cafe.pkl")
