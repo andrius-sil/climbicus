dca3d46455f751f034d33e0487ad0631b69339c5
Liucija Latanauskaite <32986613+liucijalat@users.noreply.github.com>
2020-03-18 08:26:24 +0000

    Add missing comma
    Co-Authored-By: Andrius Silinskas <silinskas.andrius@gmail.com>

--- server/app/routes/views.py
+++ server/app/routes/views.py
@@ -22,7 +22,7 @@ def route_list():
     for route in routes:
         gym_routes[route.id] = {
             "grade": route.grade,
-            "created_at": route.created_at.isoformat()
+            "created_at": route.created_at.isoformat(),
         }
 
     return jsonify({"routes": gym_routes})
