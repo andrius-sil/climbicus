ab547b10817fed14c44c5a9df127a29d9ff8259c
Andrius Silinskas <silinskas.andrius@gmail.com>
2020-07-06 21:27:28 +0100

    --tables arg for recreate-db command

--- server/app/commands.py
+++ server/app/commands.py
@@ -6,11 +6,14 @@ from scripts.dummy_db_data import preload_dummy_data
 
 
 @click.command("recreate-db")
+@click.option("--tables", type=str)
 @with_appcontext
-def recreate_db_cmd():
+def recreate_db_cmd(tables):
     db.drop_all()
     db.create_all()
     print("Initialised the database")
 
-    preload_dummy_data(db)
+    if tables is not None:
+        tables = tables.split(",")
+    preload_dummy_data(db, tables)
     print("Preloaded dummy data in the database")
