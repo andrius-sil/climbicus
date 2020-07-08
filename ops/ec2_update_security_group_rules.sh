078a52c2f5e328d5f5492ded6a238ea10e200fc0
Andrius Silinskas <silinskas.andrius@gmail.com>
2020-07-08 21:33:34 +0100

    update security rules

--- ops/ec2_update_security_group_rules.sh
+++ ops/ec2_update_security_group_rules.sh
@@ -11,14 +11,16 @@ old_ip=$(aws ec2 describe-security-groups --group-names $GROUP_NAME --query "Sec
 aws ec2 revoke-security-group-ingress --group-name $GROUP_NAME --protocol tcp --port 22 --cidr $old_ip
 aws ec2 revoke-security-group-ingress --group-name $GROUP_NAME --protocol tcp --port 5432 --cidr $old_ip
 
-if [ "$ENV" == "prod" ];then
-  flask_server_ip="0.0.0.0/0"
-else
+if [ "$ENV" == "dev" ];then
   flask_server_ip=$old_ip
+else
+  flask_server_ip="0.0.0.0/0"
 fi
+echo $flask_server_ip
 aws ec2 revoke-security-group-ingress --group-name $GROUP_NAME --protocol tcp --port 5000 --cidr $flask_server_ip
 
 # Add the rules back in with the new ip address.
-export CIDR_IP=$(curl https://ipinfo.io/ip)
-aws ec2 authorize-security-group-ingress --group-name $GROUP_NAME --ip-permissions $(cat climbicus_${ENV}_security_rules.json | envsubst | jq -c .)
+export DEFAULT_CIDR_IP="$(curl https://ipinfo.io/ip)/32"
+export SERVER_CIDR_IP=$flask_server_ip
+aws ec2 authorize-security-group-ingress --group-name $GROUP_NAME --ip-permissions $(cat climbicus_security_rules.json | envsubst | jq -c .)
 
