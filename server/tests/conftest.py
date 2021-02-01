88b4db2f8631ef06b34f39acd675d471f61e60a0
Liucija Latanauskaite <llatanauskaite@gocardless.com>
2021-02-01 22:36:02 +0100

    user_route_log: return all logs

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -142,7 +142,7 @@ def app(resource_dir):
                 ),
                 UserRouteLog(
                     route_id=1,
-                    user_id=1,
+                    user_id=2,
                     gym_id=1,
                     completed=True,
                     num_attempts=10,
--- server/tests/test_user_route_log.py
+++ server/tests/test_user_route_log.py
@@ -21,8 +21,6 @@ def test_view_logbook(client, auth_headers_user1):
               "route_id": 1, "user_id": 1},
         "2": {"completed": False, "created_at": "2012-03-04T10:10:10+00:00", "gym_id": 1, "id": 2, "num_attempts": 1,
               "route_id": 3, "user_id": 1},
-        "3": {"completed": True, "created_at": "2012-03-02T10:10:10+00:00", "gym_id": 1, "id": 3, "num_attempts": 10,
-              "route_id": 1, "user_id": 1},
     }
 
     assert expected_logbook == resp.json
@@ -40,7 +38,7 @@ def test_view_logbook_one_route(client, auth_headers_user1):
         "1": {"completed": True, "created_at": "2012-03-03T10:10:10+00:00", "gym_id": 1, "id": 1, "num_attempts": None,
               "route_id": 1, "user_id": 1},
         "3": {"completed": True, "created_at": "2012-03-02T10:10:10+00:00", "gym_id": 1, "id": 3, "num_attempts": 10,
-              "route_id": 1, "user_id": 1},
+              "route_id": 1, "user_id": 2},
     }
 
     assert expected_logbook == resp.json
