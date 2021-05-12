c57808671d2d6414112f964daac6a5fc7a62bc6e
Andrius Silinskas <andriussilinskas@opensignal.com>
2021-05-12 19:34:22 +0100

    BlocObserver & Sentry breadcrumbs

--- app/climbicus/lib/blocs/simple_bloc_delegate.dart
+++ app/climbicus/lib/blocs/simple_bloc_delegate.dart
@@ -1,18 +1,23 @@
 
 import 'package:bloc/bloc.dart';
 import 'package:flutter/foundation.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 class SimpleBlocObserver extends BlocObserver {
   @override
   void onEvent(Bloc bloc, Object? event) {
     super.onEvent(bloc, event);
     debugPrint("Event: $event");
+
+    Sentry.addBreadcrumb(Breadcrumb(message: "Event: $event", category: "bloc.event"));
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
     debugPrint("$transition");
+
+    Sentry.addBreadcrumb(Breadcrumb(message: "$transition", category: "bloc.transition"));
   }
 
   @override
