e4117367eb0e3dd856a959ffff50c7dad2af3fcc
Andrius Silinskas <andriussilinskas@opensignal.com>
2021-06-03 08:31:00 +0100

    server: GET /users accept 'user_ids'

--- server/app/users/views.py
+++ server/app/users/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 from app import db
 from app.models import Users
@@ -7,7 +7,11 @@ blueprint = Blueprint("users_blueprint", __name__, url_prefix="/users")
 
 @blueprint.route("/", methods=["GET"])
 def users_list():
+    user_ids = request.json.get("user_ids")
+
     query = db.session.query(Users)
+    if user_ids:
+        query = query.filter(Users.id.in_(user_ids))
     users = { user.id: user.api_model for user in query.all() }
 
     return jsonify({"users": users})
